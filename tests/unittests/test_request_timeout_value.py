52b5763e7bae5467e33728569be535505e826982
namrata270998 <namrata.brahmbhatt@crestdatasys.com>
2022-03-21 13:54:31 +0000

    used call_args

--- tests/unittests/test_request_timeout_value.py
+++ tests/unittests/test_request_timeout_value.py
@@ -42,11 +42,12 @@ class TestTimeoutValue(unittest.TestCase):
 
         # function call
         tap_twitter_ads.main()
+        args, kwargs = mocked_client.call_args
         # verify that the request was called with expected timeout value
-        mocked_client.assert_called_with(consumer_key='test_ck', consumer_secret='test_ck', access_token='test_at', access_token_secret='test_ts', options={'handle_rate_limit': True, 'retry_max': 10, 'retry_delay': 60000, 'retry_on_status': [400, 420, 500, 502, 503, 504], 'retry_on_timeouts': True, 'timeout': 100.0})
+        self.assertEqual(kwargs.get('options').get('timeout'), 100.0)
 
     def test_timeout_value_not_in_config(self, mocked_parse_args, mocked_client):
-        """ 
+        """
             Unit tests to ensure that request timeout is set based default value
         """
         mock_config = {'start_date': 'test_start_date',
@@ -60,9 +61,10 @@ class TestTimeoutValue(unittest.TestCase):
 
         # function call
         tap_twitter_ads.main()
+        args, kwargs = mocked_client.call_args
         # verify that the request was called with expected timeout value
-        mocked_client.assert_called_with(consumer_key='test_ck', consumer_secret='test_ck', access_token='test_at', access_token_secret='test_ts', options={'handle_rate_limit': True, 'retry_max': 10, 'retry_delay': 60000, 'retry_on_status': [400, 420, 500, 502, 503, 504], 'retry_on_timeouts': True, 'timeout': 300})
-    
+        self.assertEqual(kwargs.get('options').get('timeout'), 300)
+
     def test_timeout_string_value_in_config(self, mocked_parse_args, mocked_client):
         """ 
             Unit tests to ensure that request timeout is set based on config if string value is given in config
@@ -79,9 +81,10 @@ class TestTimeoutValue(unittest.TestCase):
 
         # function call
         tap_twitter_ads.main()
+        args, kwargs = mocked_client.call_args
         # verify that the request was called with expected timeout value
-        mocked_client.assert_called_with(consumer_key='test_ck', consumer_secret='test_ck', access_token='test_at', access_token_secret='test_ts', options={'handle_rate_limit': True, 'retry_max': 10, 'retry_delay': 60000, 'retry_on_status': [400, 420, 500, 502, 503, 504], 'retry_on_timeouts': True, 'timeout': 100.0})
-    
+        self.assertEqual(kwargs.get('options').get('timeout'), 100)
+
     def test_timeout_empty_value_in_config(self, mocked_parse_args, mocked_client):
         """ 
             Unit tests to ensure that request timeout is set based on default value if empty value is given in config
@@ -98,9 +101,10 @@ class TestTimeoutValue(unittest.TestCase):
 
         # function call
         tap_twitter_ads.main()
+        args, kwargs = mocked_client.call_args
         # verify that the request was called with expected timeout value
-        mocked_client.assert_called_with(consumer_key='test_ck', consumer_secret='test_ck', access_token='test_at', access_token_secret='test_ts', options={'handle_rate_limit': True, 'retry_max': 10, 'retry_delay': 60000, 'retry_on_status': [400, 420, 500, 502, 503, 504], 'retry_on_timeouts': True, 'timeout': 300})
-    
+        self.assertEqual(kwargs.get('options').get('timeout'), 300)
+
     def test_timeout_0_value_in_config(self, mocked_parse_args, mocked_client):
         """ 
             Unit tests to ensure that request timeout is set based on default value if 0 is given in config
@@ -117,9 +121,10 @@ class TestTimeoutValue(unittest.TestCase):
 
         # function call
         tap_twitter_ads.main()
+        args, kwargs = mocked_client.call_args
         # verify that the request was called with expected timeout value
-        mocked_client.assert_called_with(consumer_key='test_ck', consumer_secret='test_ck', access_token='test_at', access_token_secret='test_ts', options={'handle_rate_limit': True, 'retry_max': 10, 'retry_delay': 60000, 'retry_on_status': [400, 420, 500, 502, 503, 504], 'retry_on_timeouts': True, 'timeout': 300})
-    
+        self.assertEqual(kwargs.get('options').get('timeout'), 300)
+
     def test_timeout_string_0_value_in_config(self, mocked_parse_args, mocked_client):
         """ 
             Unit tests to ensure that request timeout is set based on default value if string 0  is given in config
@@ -136,8 +141,9 @@ class TestTimeoutValue(unittest.TestCase):
 
         # function call
         tap_twitter_ads.main()
+        args, kwargs = mocked_client.call_args
         # verify that the request was called with expected timeout value
-        mocked_client.assert_called_with(consumer_key='test_ck', consumer_secret='test_ck', access_token='test_at', access_token_secret='test_ts', options={'handle_rate_limit': True, 'retry_max': 10, 'retry_delay': 60000, 'retry_on_status': [400, 420, 500, 502, 503, 504], 'retry_on_timeouts': True, 'timeout': 300})
+        self.assertEqual(kwargs.get('options').get('timeout'), 300)
 
     def test_timeout_float_value_in_config(self, mocked_parse_args, mocked_client):
         """ 
@@ -155,5 +161,6 @@ class TestTimeoutValue(unittest.TestCase):
 
         # function call
         tap_twitter_ads.main()
+        args, kwargs = mocked_client.call_args
         # verify that the request was called with expected timeout value
-        mocked_client.assert_called_with(consumer_key='test_ck', consumer_secret='test_ck', access_token='test_at', access_token_secret='test_ts', options={'handle_rate_limit': True, 'retry_max': 10, 'retry_delay': 60000, 'retry_on_status': [400, 420, 500, 502, 503, 504], 'retry_on_timeouts': True, 'timeout': 100.8})
+        self.assertEqual(kwargs.get('options').get('timeout'), 100.8)

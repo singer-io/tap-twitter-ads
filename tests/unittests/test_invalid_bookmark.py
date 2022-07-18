603b1b84e0888d5128a8825f4aa00c42d4f6a32f
prijendev <prijen.khokhani@crestdatasys.com>
2022-07-18 15:25:22 +0530

    Replaced unicode character U+00a0 with normal whitespace.

--- tests/unittests/test_invalid_bookmark.py
+++ tests/unittests/test_invalid_bookmark.py
@@ -3,7 +3,7 @@ from tap_twitter_ads.streams import TwitterAds
 
 class TestBookmark(unittest.TestCase):
     """
-    Test the get_bookmark method for different values
+    Test the get_bookmark method for different values
     """
     stream = "dummy_stream"
     default = "2018-01-28T00:00:00Z"
@@ -11,7 +11,7 @@ class TestBookmark(unittest.TestCase):
     twitter_ads_object = TwitterAds()
     
     def test_empty_bookmark(self):
-        """ Test that if an empty state passed in get_bookmark then it returns the default value"""
+        """ Test that if an empty state passed in get_bookmark then it returns the default value"""
         state = {}
         
         bookmark = self.twitter_ads_object.get_bookmark(state, self.stream, self.default, self.account_id)
@@ -19,7 +19,7 @@ class TestBookmark(unittest.TestCase):
         self.assertEqual(bookmark, self.default)
 
     def test_invalid_bookmark(self):
-        """ Test that if the `bookmarks` key does not found in the state then it returns the default value """
+        """ Test that if the `bookmarks` key does not found in the state then it returns the default value """
 
         state = {'bookmark_1': {}}
         
@@ -28,7 +28,7 @@ class TestBookmark(unittest.TestCase):
         self.assertEqual(bookmark, self.default)
 
     def test_valid_bookmark(self):
-        """ Test that if the valid bookmark is available in the state then it returns the bookmark value """
+        """ Test that if the valid bookmark is available in the state then it returns the bookmark value """
 
         state = {'bookmarks': {self.stream: {self.account_id: "2017-01-28T00:00:00Z"}}}
         
@@ -37,7 +37,7 @@ class TestBookmark(unittest.TestCase):
         self.assertEqual(bookmark, "2017-01-28T00:00:00Z")
 
     def test_null_bookmark(self):
-        """ Test that if the bookmark value is None in the state then it returns None"""
+        """ Test that if the bookmark value is None in the state then it returns None"""
 
         state = {'bookmarks': {self.stream: {self.account_id: None}}}
         

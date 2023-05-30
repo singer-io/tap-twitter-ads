ca124649d68083f8c2cf02714636f014e86418ea
Andy Lu <andy@stitchdata.com>
2023-05-30 20:26:26 +0000

    Bump version, update changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-twitter-ads',
-      version='1.0.0',
+      version='1.0.1',
       description='Singer.io tap for extracting data from the Twitter Ads API API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
@@ -12,7 +12,7 @@ setup(name='tap-twitter-ads',
           'backoff==1.8.0',
           'requests==2.31.0',
           'singer-python==5.9.0',
-          'twitter-ads==11.0.0'
+          'twitter-ads==11.0.1'
       ],
       extras_require={
           'dev': [
